1c6dc5a263b4cc8336dd73adf2ea56a2afb1abc0
榊原昌彦 <sakakibara@rdlabo.jp>
2024-03-14 20:34:39 +0900

    chore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import {Component, signal} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logoInstagram, logoFacebook, callOutline, timeOutline, lockClosedOutline, mapOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgOptimizedImage],
+  imports: [CommonModule, RouterOutlet, NgOptimizedImage, IonIcon],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss', './helper.scss']
 })
 export class AppComponent {
   title = 'concent-market';
   heroImagePath = signal('/assets/images/IMG_6931.JPG');
+
+  constructor() {
+    addIcons({logoInstagram, logoFacebook, callOutline, timeOutline, lockClosedOutline, mapOutline})
+  }
 
   changeHeroImage(path: string) {
     this.heroImagePath = signal(path);
